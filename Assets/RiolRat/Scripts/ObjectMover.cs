bef54a8d8cf245518d4f0c73d049d66e54d99d0c
jam54 <git@jam54.com>
2020-05-02 18:25:08 +0000

    Gefixt da peetje blijft hangen, object mover script toegevoegd

---
+++ Assets/RiolRat/Scripts/ObjectMover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    private Vector3 StartPosition;
+    public Vector3 EndPosition;
+    public float GaanSnelheid;
+    public float TerugSnelheid;
+
+    public AudioClip GeluidOpImpact;
+    public AudioClip GeluidBijTerugGaan;
+
+    private bool AanHetGaan = true; // Als het terug komt, gwn op false zetten
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, EndPosition) < 0.1f)
+        {
+            AanHetGaan = false;
+            //geluid op impact hier
+        }
+
+        if (Vector3.Distance(this.transform.position, StartPosition) < 0.1f)
+        {
+            AanHetGaan = true;
+        }
+
+        if (AanHetGaan)
+        {
+            this.transform.position = Vector3.MoveTowards(transform.position, EndPosition, GaanSnelheid * Time.deltaTime); //Vector3.MoveTowards(this.transform.position, EndPosition, 0.000f);
+        }
+
+        if (!AanHetGaan)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, StartPosition, TerugSnelheid * Time.deltaTime);
+        }
+
+    }
+}
