9e8316e10b65ce52c3cc66f22effd757c1186366
jam54 <git@jam54.com>
2020-05-05 21:01:08 +0000

    coin toegevoegd

---
+++ Assets/RiolRat/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // If the player enters the trigger zone...
+        if (other.tag == "Player")
+        {
+            // Destroy the crate.
+            Destroy(gameObject);
+            Debug.Log("You got 1 coin!");
+        }
+        
+    }
+
+}
