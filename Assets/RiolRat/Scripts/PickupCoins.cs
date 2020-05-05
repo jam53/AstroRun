739cb5a282ab044ccad1733627f043b044f4e2ab
Deniz Callens <deniz.callens@outlook.com>
2020-05-05 23:54:10 +0000

    No Comments

---
+++ Assets/RiolRat/Scripts/PickupCoins.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using System;
+
+public class PickupCoins : MonoBehaviour
+{
+    public TextMeshProUGUI CoinsText;
+
+    private int Coins;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // If the player enters the trigger zone...
+        if (other.tag == "Coin")
+        {
+            // Destroy the crate.
+            Destroy(other.gameObject);
+            Coins++;
+            Debug.Log("Player has: " + Coins + " coins.");
+            CoinsText.text = Convert.ToString(Coins);
+        }
+        
+    }
+
+}
