17ffdf181f4636cf8963204f9bb605f8da1b9427
Deniz Callens <deniz.callens@outlook.com>
2020-05-12 19:52:29 +0000

    No Comments

---
+++ Assets/RiolRat/Scripts/Trampoline.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class Trampoline : MonoBehaviour
+{
+    public int speed;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * speed);
+    }
+
+}
