0d9e0d9c4afb396fef6066f9551c34b02529f74d
jam54 <git@jam54.com>
2020-05-14 10:15:08 +0000

    Springs toegevoegd

--- Assets/RiolRat/Scripts/Trampoline.cs
+++ Assets/RiolRat/Scripts/Trampoline.cs
@@ -6,9 +6,13 @@ using UnityStandardAssets._2D;
 public class Trampoline : MonoBehaviour
 {
     public int speed;
+    public Animator animator;
+    public AudioSource audioSource;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        animator.SetTrigger("PlayerTouched");
+        audioSource.Play();
         other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * speed);
     }
 
