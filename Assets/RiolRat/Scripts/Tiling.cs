7ccf2d434594ee397657a23b5775263a771927fd
jam54 <git@jam54.com>
2020-05-14 13:30:18 +0000

    parallaxbackground toegevoegd

---
+++ Assets/RiolRat/Scripts/Tiling.cs
@@ -0,0 +1,80 @@
+﻿using Boo.Lang.Environments;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour
+{
+    public int offSetX = 2;
+
+    public bool hasARightBuddy = false;
+    public bool hasALeftBuddy = false;
+
+    public bool reverseScale = false;
+
+    private float spriteWidth = 0f;
+    private Camera cam;
+    private Transform myTransform;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        myTransform = transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+        spriteWidth = sRenderer.sprite.bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasALeftBuddy == false || hasARightBuddy == false)
+        {
+            float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+
+            float edgeVisiblePositionRight = (myTransform.position.x + spriteWidth/2) - camHorizontalExtend;
+            float edgeVisiblePositionLeft = (myTransform.position.x - spriteWidth/2) + camHorizontalExtend;
+
+            if (cam.transform.position.x >= edgeVisiblePositionRight - offSetX && hasARightBuddy == false)
+            {
+                MakeNewBuddy(1);
+                hasARightBuddy = true;
+            }
+
+            else if (cam.transform.position.x <= edgeVisiblePositionLeft + offSetX && hasALeftBuddy == false)
+            {
+                MakeNewBuddy(-1);
+                hasALeftBuddy = true;
+            }
+        }
+    }
+
+    private void MakeNewBuddy(int rightOrLeft)
+    {
+        Vector3 newPosition = new Vector3(myTransform.position.x + spriteWidth * rightOrLeft, myTransform.position.y, myTransform.position.z);
+        Transform newBuddy = Instantiate(myTransform, newPosition, myTransform.rotation);
+
+        if (reverseScale == true)
+        {
+            newBuddy.localScale = new Vector3(newBuddy.localScale.x * -1, newBuddy.localScale.y, newBuddy.localScale.z);
+        }
+
+        //newBuddy.parent = myTransform.parent;
+        newBuddy.parent = myTransform;
+        if (rightOrLeft > 0)
+        {
+            newBuddy.GetComponent<Tiling>().hasALeftBuddy = true;
+        }
+
+        else
+        {
+            newBuddy.GetComponent<Tiling>().hasARightBuddy = true;
+        }
+    }
+}
