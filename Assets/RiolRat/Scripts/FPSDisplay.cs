8d4d084ec6964d0cb836016570635607a9e1c7be
Deniz Callens <deniz.callens@outlook.com>
2020-05-26 13:04:45 +0000

    FPS counter + portal naar level 3 toegevoegd

---
+++ Assets/RiolRat/Scripts/FPSDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public int avgFrameRate;
+    public TextMeshProUGUI display_TextFPS;
+    public TextMeshProUGUI display_TextMaxFPS;
+
+    private int MaxFPS = 0;
+
+    public void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_TextFPS.text = avgFrameRate.ToString() + " FPS";
+        if (MaxFPS < avgFrameRate)
+        {
+            MaxFPS = avgFrameRate;
+            display_TextMaxFPS.text = "MAX " + MaxFPS.ToString() + " FPS";
+        }
+    }
+}
