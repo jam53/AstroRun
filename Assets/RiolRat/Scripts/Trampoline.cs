f6bcb8c7d2e05acfb9b92202aee1f3db0c17f418
Deniz Callens <deniz.callens@outlook.com>
2020-06-05 22:19:25 +0000

    Fixed a bug which caused the wrong spring to animate

--- Assets/RiolRat/Scripts/Trampoline.cs
+++ Assets/RiolRat/Scripts/Trampoline.cs
@@ -11,9 +11,19 @@ public class Trampoline : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        animator.SetTrigger("PlayerTouched");
-        audioSource.Play();
         other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * speed);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetTrigger("PlayerTouched");
+        audioSource.Play();
+    }
+
+    // We hebben een oncollision en een ontrigger. 
+    // Want als we enkel oncollision doen, en de player sprint tussen 2 springs, dan gaat hij dubbel zo hoog
+    // Dus één spring als enige heeft een collider is trigger false heeft staan.
+    // Deze geeft de boost, hij heeft ook nog een collider met istrigger zoals alle anderen
+    // En dat zorgt voor de animatie
 
 }
