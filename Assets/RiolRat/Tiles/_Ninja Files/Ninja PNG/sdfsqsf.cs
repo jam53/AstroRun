aa617a1950f35507e1fc005b4fb766d6a9709d37
Deniz Callens <deniz.callens@outlook.com>
2020-06-05 12:12:00 +0000

    Upgedate naar 2019.3.15f1 en nieuwe achtergrond muziek + nieuwe checkpoint + spike inslag

---
+++ Assets/RiolRat/Tiles/_Ninja Files/Ninja PNG/sdfsqsf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sdfsqsf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }//qsdfqs
+}// Texture atlas maken, of sprite sheet
+// want al die fotos appart, komen wss groter uit dan 1 sprite sheet of atlas
+// en in 1 sprite sheet ook betere performance wss
+//https://gamedevelopment.tutsplus.com/articles/using-texture-atlas-in-order-to-optimize-your-game--cms-26783
