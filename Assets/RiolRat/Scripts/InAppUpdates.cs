75311611a40c44651388795121ef7b63e88c21df
Deniz Callens <deniz.callens@outlook.com>
2020-07-16 15:50:41 +0000

    Added InAppUpdates UI

---
+++ Assets/RiolRat/Scripts/InAppUpdates.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.RemoteConfig;
+
+public class InAppUpdates : MonoBehaviour
+{
+
+    public struct userAttributes { }
+
+    public struct appAttributes { }
+
+    public bool LevelCanBeLoaded = false;
+
+    private void Awake()
+    {
+        ConfigManager.FetchCompleted += CheckForUpdates;
+        ConfigManager.FetchConfigs<userAttributes, appAttributes>(new userAttributes(), new appAttributes());
+    }
+
+    void CheckForUpdates (ConfigResponse response)
+    {
+        if (ConfigManager.appConfig.GetString("ApplicationVersion") != Application.version)
+        {
+            Application.OpenURL("https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=com.jam54.AstroRun");
+        }
+
+        else
+        {
+            LevelCanBeLoaded = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
