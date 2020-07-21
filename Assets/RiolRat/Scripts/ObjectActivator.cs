79dfb547c12b917ff99c75eea2c7190978293136
Deniz Callens <deniz.callens@outlook.com>
2020-07-21 09:31:02 +0000

    Bug op level 2 gefixed, die ervoor zorge dat je mega hoog sprong op een bepaalde spring

---
+++ Assets/RiolRat/Scripts/ObjectActivator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectActivator : MonoBehaviour
+{
+    public string ActivatorTag;
+
+    public bool DeactivateOnExit;
+
+    public GameObject[] Objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var item in Objects)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == ActivatorTag)
+        {
+            foreach (var item in Objects)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == ActivatorTag && DeactivateOnExit)
+        {
+            foreach (var item in Objects)
+            {
+                item.SetActive(false);
+            }
+        }
+    }
+}
