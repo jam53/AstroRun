c84b384d0ea8a597f0596903d8e50dc8a0572399
jam54 <git@jam54.com>
2020-08-12 12:32:46 +0000

    Added a level timer, so your best time gets saved Nog wa dingen aan Main Menu gedaan en verbeterd UI van Main Menu beetje aangepast, zodat als er grote getallen zijn, dier nog steeds correct worden weergegeven Je kan nu terug naar het main menu vanaf het pause menu Level selection werkt About werkt Als je bepaalde levels nog niet hebt unlocked, kan je ze niet kiezen Als je een coin ooit hebt opgepakt, kan je die niet meer opnieuw oppakken

---
+++ Assets/RiolRat/Scripts/LevelSelection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour
+{
+    public Button[] LevelButtons;
+
+    public Sprite LockedIcon;
+
+    private int LevelAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelAt = Convert.ToInt32(GPGSAutenthicator.GPGSZelf.LoadString(10));
+
+        for (int i = LevelAt; i < LevelButtons.Length; i++)
+        {
+            LevelButtons[i].interactable = false;
+            LevelButtons[i].image.sprite = LockedIcon;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
