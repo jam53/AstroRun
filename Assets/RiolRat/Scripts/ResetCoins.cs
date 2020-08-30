02a827736994523faf74355a39551b427573d5b0
Deniz Callens <deniz.callens@outlook.com>
2020-08-30 12:04:32 +0000

    Added A gender switch button + skins in juiste volgorde

---
+++ Assets/RiolRat/Scripts/ResetCoins.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetCoins : MonoBehaviour
+{
+    private DateTime _lastAdTime = DateTime.MinValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lastAdTime.AddDays(1);
+        Debug.Log(_lastAdTime);
+        Debug.Log(_lastAdTime = DateTime.Now);
+        Debug.Log(_lastAdTime.AddDays(1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SellMeStuff()
+    {
+        if (_lastAdTime.AddDays(1) > DateTime.Now)
+        {
+            Debug.Log("Buy from me, please?");
+            // Store for next time
+            _lastAdTime = DateTime.Now;
+        }
+    }
+}
