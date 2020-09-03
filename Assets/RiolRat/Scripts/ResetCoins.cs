a8adb7f4282cfb88ecf040b10c723983c9f1c3d9
Deniz Callens <deniz.callens@outlook.com>
2020-09-03 17:21:50 +0000

    No Comments

--- Assets/RiolRat/Scripts/ResetCoins.cs
+++ Assets/RiolRat/Scripts/ResetCoins.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class ResetCoins : MonoBehaviour
