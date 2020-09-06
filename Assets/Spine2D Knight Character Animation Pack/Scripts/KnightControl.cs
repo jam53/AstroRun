d41ad611bea90d6afe9b6ac4d9622b16e3f4b2b9
Deniz Callens <deniz.callens@outlook.com>
2020-09-06 14:47:39 +0000

    Spine 2D Character Animation + Packages upgedate

---
+++ Assets/Spine2D Knight Character Animation Pack/Scripts/KnightControl.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine;
+using Spine.Unity;
+
+public class KnightControl : MonoBehaviour
+{
+    #region Inspector
+    // [SpineAnimation] attribute allows an Inspector dropdown of Spine animation names coming form SkeletonAnimation.
+    [SpineAnimation]
+    public string runAnimationName;
+
+    [SpineAnimation]
+    public string idleAnimationName;
+
+    [SpineAnimation]
+    public string walkAnimationName;
+
+    [SpineAnimation]
+    public string atkAnimationName_1;
+
+    [SpineAnimation]
+    public string atkAnimationName_2;
+
+    [SpineAnimation]
+    public string jumpAnimationName;
+
+    [SpineAnimation]
+    public string hitAnimationName;
+
+    [SpineAnimation]
+    public string deathAnimationName;
+
+    [SpineAnimation]
+    public string stunAnimationName;
+
+    [SpineAnimation]
+    public string skillAnimationName_1;
+    [SpineAnimation]
+    public string skillAnimationName_2;
+    [SpineAnimation]
+    public string skillAnimationName_3;
+
+    #endregion
+
+    SkeletonAnimation skeletonAnimation;
+
+    // Spine.AnimationState and Spine.Skeleton are not Unity-serialized objects. You will not see them as fields in the inspector.
+    public Spine.AnimationState spineAnimationState;
+    public Spine.Skeleton skeleton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        spineAnimationState = skeletonAnimation.AnimationState;
+        skeleton = skeletonAnimation.Skeleton;
+    }
+
+    public void running()
+    {
+        spineAnimationState.SetAnimation(0, runAnimationName, true);
+    }
+    public void walking()
+    {
+        spineAnimationState.SetAnimation(0, walkAnimationName, true);
+    }
+    public void idle()
+    {
+        spineAnimationState.SetAnimation(0, idleAnimationName, true);
+    }
+    public void jump()
+    {
+        spineAnimationState.SetAnimation(0, jumpAnimationName, true);
+    }
+    public void getHit()
+    {
+        spineAnimationState.SetAnimation(0, hitAnimationName, true);
+    }
+    public void death()
+    {
+        spineAnimationState.SetAnimation(0, deathAnimationName, true);
+    }
+    public void stun()
+    {
+        spineAnimationState.SetAnimation(0, stunAnimationName, true);
+    }
+    public void attack_1()
+    {
+        spineAnimationState.SetAnimation(0, atkAnimationName_1, true);
+    }
+    public void attack_2()
+    {
+        spineAnimationState.SetAnimation(0, atkAnimationName_2, true);
+    }
+    public void skill_1()
+    {
+        spineAnimationState.SetAnimation(0, skillAnimationName_1, true);
+    }
+    public void skill_2()
+    {
+        spineAnimationState.SetAnimation(0, skillAnimationName_2, true);
+    }
+    public void skill_3()
+    {
+        spineAnimationState.SetAnimation(0, skillAnimationName_3, true);
+    }
+
+}
