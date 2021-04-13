612958291a6893def0d94132a2151bff66b65494
jam53 <deniz.callens@outlook.com>
2021-04-13 15:37:33 +0200

    Changes to tiles + Texture optimazations + Stripped/Disabled Post Processing + Optimized Riol Rat audio file
    Removed double tiles from the tile pallete
    Moved the tiles that looked the same to the "Dubbel" Folder
    Added sprite atlasses
    Reduced the resolution of certain textures
    Added a new tile to the Autumn tileset
    Took a short part from the Riol Rat audio clip, and made it loopable. And therefore smaller in size
    Removed the folder Assets/RiolRat/Tiles/_BayatGames/Free Platform Game Assets/Updates/Update 1.9/New Details since it contained the exact same sprites as in Assets/RiolRat/Tiles/_BayatGames/Free Platform Game Assets/Environments/New Details ( Update 1.9 )

---
+++ Assets/RiolRat/Scripts/AudioIntroAndLoop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class AudioIntroAndLoop : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip Intro;
+    public AudioClip Loop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = Intro;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying && Time.timeSinceLevelLoad > 2)
+        {
+            audioSource.clip = Loop;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
