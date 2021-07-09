343a2788aeafb97e6dd95e176b839f15cf994ca3
jam53 <deniz.callens@outlook.com>
2021-07-09 11:30:43 +0200

    Worked on dialogue boxes
    Added a new dialogue box that explains to the user that they can unlock new levels by buying them
    
    Extended the functionality of the dialogue box. You can now state if it should be showed every time it's triggerd, or just once.

--- Assets/RiolRat/Scripts/DialogueTrigger.cs
+++ Assets/RiolRat/Scripts/DialogueTrigger.cs
@@ -4,9 +4,75 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    public bool RunOnlyOnce;
+
+    public bool RunWhenActivedInHierachy;
+
+    public int SavingID;
+
+
+
     public Dialogue dialogue;
 
-    public void TriggerDialogue()
+    public void Start()
+    {
+        if (RunWhenActivedInHierachy)
+        {
+            if (!RunOnlyOnce)
+            {
+                DialogueManager dialogueManager = FindObjectOfType<DialogueManager>();
+
+                dialogueManager.StartDialogue(dialogue);
+            }
+
+            else if (RunOnlyOnce)//Show the dialogue window only one time, then never show it again to the user
+            {
+                if (!GPGSAutenthicator.GPGSZelf.LoadBool(SavingID))
+                {
+                    DialogueManager dialogueManager = FindObjectOfType<DialogueManager>();
+
+                    dialogueManager.StartDialogue(dialogue);
+
+                    GPGSAutenthicator.GPGSZelf.Save(SavingID, true);
+                }
+
+                else if (GPGSAutenthicator.GPGSZelf.LoadBool(SavingID))
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    public void TriggerDialogueCheckIfRanBefore()//Only open the dialogue when calling his function if
+        //                                         It hasn't been opened before
+    {
+        if (!RunOnlyOnce)
+        {
+            DialogueManager dialogueManager = FindObjectOfType<DialogueManager>();
+
+            dialogueManager.StartDialogue(dialogue);
+        }
+
+        else if (RunOnlyOnce)//Show the dialogue window only one time, then never show it again to the user
+        {
+            if (!GPGSAutenthicator.GPGSZelf.LoadBool(SavingID))
+            {
+                DialogueManager dialogueManager = FindObjectOfType<DialogueManager>();
+
+                dialogueManager.StartDialogue(dialogue);
+
+                GPGSAutenthicator.GPGSZelf.Save(SavingID, true);
+            }
+
+            else if (GPGSAutenthicator.GPGSZelf.LoadBool(SavingID))
+            {
+                return;
+            }
+        }
+    }
+
+    public void TriggerDialogue()//Open The dialogue when calling this function
     {
         DialogueManager dialogueManager = FindObjectOfType<DialogueManager>();
 
